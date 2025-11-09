8fa70ab82513dc595a0d4c842fc1833c52b9a42b
tyler-mcgrew-meng <mengt@wustl.edu>
2025-11-09 07:47:05 -0600

    fix line 20 interps

--- amplitudes/line20_amp.m
+++ amplitudes/line20_amp.m
@@ -7,14 +7,9 @@ imo_long_x = line20_x;
 
 imo_long_t = line20_t;
 amplitude = line20;
-% 
-% imo_long_topo = rmmissing(readtable('line14_topo_hires.csv')); topo_z = (imo_long_topo.z);
-% topo_x = imo_long_topo.x;
-% topo_E = imo_long_topo.E;
-% topo_N = imo_long_topo.N;
 
-% debris_interp = readtable('interp/line147-18_debris.csv', 'NumHeaderLines',3);
-base_interp = readtable('line20_interps.csv', 'NumHeaderLines',3);
+
+base_interp = readtable('line20_interps.csv', 'NumHeaderLines',2);
 
 %assumed or measured velocity (m/ns)
 v1 = 0.17;
@@ -50,9 +45,9 @@ power = zeros(size(imo_long_x));
 
 dt = imo_long_t(2)-imo_long_t(1);
 
-win_size = 20;
+win_size = 10;
 
-for i = 3:length(imo_long_x)-2
+for i = 3:length(imo_long_x)-10
     window = imo_long_t(base_trace(i)-imo_long_t < win_size & base_trace(i)-imo_long_t > - win_size);
     
     amps1 = amplitude(:,i);
@@ -74,14 +69,14 @@ for i = 3:length(imo_long_x)-2
         pow = []; amps = []; amps1 = []; window = []; t_window = [];
        
 end
-
+t_max = t_max(t_max > 0);
 %plots
 figure(2);
 pcolor(imo_long_x, imo_long_depth, imo_long_gain); hold on;
 shading interp; 
 plot(base_interp.Position_m_, base_interp.Depth_m_/v_int*v1, 'r-','LineWidth',4);
 
-plot(imo_long_x(2:96),0.5*t_max*v1, 'm*','LineWidth',4);
+plot(imo_long_x(3:491),0.5*t_max*v1, 'm*','LineWidth',4);
 % plot(debris_interp.Position_m_, debris_interp.Depth_m_/0.1*v1, 'r-','LineWidth',2);
 ylim([0 50]);
 caxis([-5e6 5e6]);
@@ -93,27 +88,27 @@ trace_b = 86;
 
 figure(4);
 subplot 121
-plot((10*log10(line14(:,trace_a).^2)),0.5*v1*line14_t,'k','LineWidth',2); hold on;
+plot((10*log10(amplitude(:,trace_a).^2)),0.5*v1*imo_long_t,'k','LineWidth',2); hold on;
 plot(10*log10(power(trace_a)),0.5*t_max(trace_a)*v1, 'r*');
 axis ij; 
 ylim([0 50]);
 
 subplot 122
-plot((10*log10(line14(:,trace_b).^2)),0.5*v1*line14_t,'k','LineWidth',2); hold on;
+plot((10*log10(amplitude(:,trace_b).^2)),0.5*v1*imo_long_t,'k','LineWidth',2); hold on;
 
 plot(10*log10(power(trace_b)),0.5*t_max(trace_b)*v1, 'r*');axis ij; 
 ylim([0 50]);
 
 figure(5);
-pcolor(line14_x, 0.5*v1*line14_t, 10*log10(line14.^2)); hold on;
+pcolor(imo_long_x, 0.5*v1*imo_long_t, 10*log10(amplitude.^2)); hold on;
 plot([trace_a trace_a],[0 50],'r','LineWidth',2); hold on;
 plot([trace_b trace_b],[0 50],'r','LineWidth',2);
-plot(imo_long_x(2:96), 0.5*v1*t_max,'r*', 'LineWidth', 4);
+plot(imo_long_x(3:491), 0.5*v1*t_max,'r*', 'LineWidth', 4);
 
 shading flat; 
 % plot(base_interp.Position_m_, base_interp.Depth_m_/v_int*v1, 'r-','LineWidth',4);
 % plot(debris_interp.Position_m_, debris_interp.Depth_m_/0.1*v1, 'r-','LineWidth',2);
-ylim([0 50]);
+ylim([0 20]);
 caxis([60 90]);
 axis ij;
 colormap parula;
@@ -133,18 +128,18 @@ geometric_correction4 = geometric_correction4./max(geometric_correction4);
 
 
 figure(6);
-scatter(base_depth(3:78),10*log10(power(3:78)),'k');hold on;
-scatter(base_depth(3:78),10*log10(geometric_correction2(3:78)),'r');
-scatter(base_depth(3:78),10*log10(geometric_correction3(3:78)),'b');
-scatter(base_depth(3:78),10*log10(geometric_correction4(3:78)),'c');
+scatter(base_depth(3:491),10*log10(power(3:491)),'k');hold on;
+scatter(base_depth(3:491),10*log10(geometric_correction2(3:491)),'r');
+scatter(base_depth(3:491),10*log10(geometric_correction3(3:491)),'b');
+scatter(base_depth(3:491),10*log10(geometric_correction4(3:491)),'c');
 
 
-fit = polyfit(base_depth(3:78),10*log10(power(3:78)),1);
-fit2 = polyfit(base_depth(3:78),10*log10(geometric_correction2(3:78)),1);
-fit3 = polyfit(base_depth(3:78),10*log10(geometric_correction3(3:78)),1);
-fit4 = polyfit(base_depth(3:78),10*log10(geometric_correction4(3:78)),1);
+fit = polyfit(base_depth(3:491),10*log10(power(3:491)),1);
+fit2 = polyfit(base_depth(3:491),10*log10(geometric_correction2(3:491)),1);
+fit3 = polyfit(base_depth(3:491),10*log10(geometric_correction3(3:491)),1);
+fit4 = polyfit(base_depth(3:491),10*log10(geometric_correction4(3:491)),1);
 
-X = 0:40;
+X = 0:20;
 plot(X, fit(1)*X+fit(2),'k');
 plot(X, fit2(1)*X+fit2(2),'r');
 plot(X, fit3(1)*X+fit3(2),'b');
@@ -153,8 +148,8 @@ plot(X, fit4(1)*X+fit4(2),'c');
 xlabel('Depth (m)');
 ylabel('10log_{10}(power)');
 legend('No correction','R^{2}','R^{3}','R^{4}','Location','southwest');
-xlim([10 40]);
-ylim([-30 0]);
+% xlim([10 40]);
+% ylim([-30 0]);
 
 
 % %account for geometric spreading loss 
